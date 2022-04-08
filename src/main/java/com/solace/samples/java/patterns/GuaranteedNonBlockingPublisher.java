64c660136f7c2d55775ca181c6fb80ae9a6a73e9
aaron-613 <github@aaronlee.ca>
2022-04-08 20:24:39 +0800

    initial check-in, new persistent pub sample

---
+++ src/main/java/com/solace/samples/java/patterns/GuaranteedNonBlockingPublisher.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2021 Solace Corporation. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.solace.samples.java.patterns;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.solace.messaging.MessagingService;
+import com.solace.messaging.config.SolaceProperties.AuthenticationProperties;
+import com.solace.messaging.config.SolaceProperties.ServiceProperties;
+import com.solace.messaging.config.SolaceProperties.TransportLayerProperties;
+import com.solace.messaging.config.profile.ConfigurationProfile;
+import com.solace.messaging.publisher.OutboundMessage;
+import com.solace.messaging.publisher.OutboundMessageBuilder;
+import com.solace.messaging.publisher.PersistentMessagePublisher;
+import com.solace.messaging.publisher.PersistentMessagePublisher.MessagePublishReceiptListener;
+import com.solace.messaging.publisher.PersistentMessagePublisher.PublishReceipt;
+import com.solace.messaging.resources.Topic;
+
+/**
+ * A more performant sample that shows an application that publishes.
+ */
+public class GuaranteedNonBlockingPublisher {
+    
+    private static final String SAMPLE_NAME = GuaranteedNonBlockingPublisher.class.getSimpleName();
+    private static final String TOPIC_PREFIX = "solace/samples/";  // used as the topic "root"
+    private static final String API = "Java";
+    private static final int APPROX_MSG_RATE_PER_SEC = 100;
+    private static final int PAYLOAD_SIZE = 100;
+    
+    private static volatile int msgSentCounter = 0;                   // num messages sent
+    private static volatile boolean isShutdown = false;
+    
+    private static final Logger logger = LogManager.getLogger();  // log4j2, but could also use SLF4J, JCL, etc.
+
+    /** Main method. */
+    public static void main(String... args) throws IOException {
+        if (args.length < 3) {  // Check command line arguments
+            System.out.printf("Usage: %s <host:port> <message-vpn> <client-username> [password]%n%n", SAMPLE_NAME);
+            System.exit(-1);
+        }
+        System.out.println(API + " " + SAMPLE_NAME + " initializing...");
+
+        final Properties properties = new Properties();
+        properties.setProperty(TransportLayerProperties.HOST, args[0]);          // host:port
+        properties.setProperty(ServiceProperties.VPN_NAME,  args[1]);     // message-vpn
+        properties.setProperty(AuthenticationProperties.SCHEME_BASIC_USER_NAME, args[2]);      // client-username
+        if (args.length > 3) {
+            properties.setProperty(AuthenticationProperties.SCHEME_BASIC_PASSWORD, args[3]);  // client-password
+        }
+        //properties.setProperty(JCSMPProperties.GENERATE_SEQUENCE_NUMBERS, true);  // not required, but interesting
+        properties.setProperty(TransportLayerProperties.RECONNECTION_ATTEMPTS, "20");  // recommended settings
+        properties.setProperty(TransportLayerProperties.CONNECTION_RETRIES_PER_HOST, "5");
+        // https://docs.solace.com/Solace-PubSub-Messaging-APIs/API-Developer-Guide/Configuring-Connection-T.htm
+        // ready to connect now
+        final MessagingService messagingService = MessagingService.builder(ConfigurationProfile.V1)
+                .fromProperties(properties)
+                .build();
+        messagingService.connect();  // blocking connect
+        messagingService.addServiceInterruptionListener(serviceEvent -> {
+            System.out.println("### SERVICE INTERRUPTION: "+serviceEvent.getCause());
+            //isShutdown = true;
+        });
+        messagingService.addReconnectionAttemptListener(serviceEvent -> {
+            System.out.println("### RECONNECTING ATTEMPT: "+serviceEvent);
+        });
+        messagingService.addReconnectionListener(serviceEvent -> {
+            System.out.println("### RECONNECTED: "+serviceEvent);
+        });
+        
+        // build the publisher object
+        final PersistentMessagePublisher publisher = messagingService.createPersistentMessagePublisherBuilder()
+                .onBackPressureWait(1)
+                .build();
+        publisher.start();
+        
+        publisher.setMessagePublishReceiptListener(new MessagePublishReceiptListener() {
+			@Override
+			public void onPublishReceipt(PublishReceipt publishReceipt) {
+				if (publishReceipt.isPersisted()) {  // success!  ACK received
+					
+				} else {  // failure!  NACK received
+					System.out.println("NACK received for message "+publishReceipt.getMessage()+": "+publishReceipt.getException());
+				}
+			}
+        });
+        
+        // make a thread for printing message rate stats
+        ScheduledExecutorService statsPrintingThread = Executors.newSingleThreadScheduledExecutor();
+        statsPrintingThread.scheduleAtFixedRate(() -> {
+            System.out.printf("Published msgs/s: %,d%n",msgSentCounter);  // simple way of calculating message rates
+            msgSentCounter = 0;
+        }, 1, 1, TimeUnit.SECONDS);
+
+        System.out.println(SAMPLE_NAME + " connected, and running. Press [ENTER] to quit.");
+        byte[] payload = new byte[PAYLOAD_SIZE];  // preallocate memory, for reuse, for performance
+        OutboundMessageBuilder messageBuilder = messagingService.messageBuilder();
+        // block the main thread, waiting for a quit signal
+        while (System.in.available() == 0 && !isShutdown) {
+            try {
+                // each loop, change the payload, less trivial
+                char chosenCharacter = (char)(Math.round(msgSentCounter % 26) + 65);  // rotate through letters [A-Z]
+                Arrays.fill(payload,(byte)chosenCharacter);  // fill the payload completely with that char
+                messageBuilder.withApplicationMessageId(UUID.randomUUID().toString());
+//                messageBuilder.withProperty(MessageProperties.APPLICATION_MESSAGE_ID, UUID.randomUUID().toString());  // as an example of a header
+                OutboundMessage message = messageBuilder.build(payload);  // binary payload message
+                // dynamic topics!!
+                String topicString = new StringBuilder(TOPIC_PREFIX).append(API.toLowerCase())
+                		.append("/per2s/pub/").append(chosenCharacter).toString();
+                publisher.publish(message,Topic.of(topicString));  // send the message
+                msgSentCounter++;  // add one
+            } catch (RuntimeException e) {  // threw from send(), only thing that is throwing here, but keep trying (unless shutdown?)
+                System.out.printf("### Caught while trying to publisher.publish(): %s%n",e);
+                isShutdown = true;
+            } finally {
+                try {
+                    Thread.sleep(0);//1000 / APPROX_MSG_RATE_PER_SEC);  // do Thread.sleep(0) for max speed
+                    // Note: STANDARD Edition Solace PubSub+ broker is limited to 10k msg/s max ingress
+                } catch (InterruptedException e) {
+                    isShutdown = true;
+                }
+            }
+        }
+        isShutdown = true;
+        statsPrintingThread.shutdown();  // stop printing stats
+        publisher.terminate(1500);  // allow time for ACKs/NACKs to propagate from broker
+        messagingService.disconnect();
+        System.out.println("Main thread quitting.");
+    }
+}
